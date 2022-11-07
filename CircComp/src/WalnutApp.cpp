7532ce2503d9d69cdf99c110bce6d719e94a92f5
polyqubit <polyumbrial@gmail.com>
2022-11-06 16:52:32 -0800

    pixels

--- CircComp/src/WalnutApp.cpp
+++ CircComp/src/WalnutApp.cpp
@@ -2,6 +2,8 @@
 #include "Walnut/EntryPoint.h"
 
 #include "Walnut/Image.h"
+#include "Walnut/Random.h"
+#include "Walnut/Timer.h"
 
 class ExampleLayer : public Walnut::Layer
 {
@@ -9,14 +11,53 @@ public:
 	virtual void OnUIRender() override
 	{
 		ImGui::Begin("Hello");
-		if (ImGui::Button("test"))
 		{
-			ImGui::Text("Wow!!");
+			ImGui::Text("Time taken to render: %.3fms", m_ElapsedTime);
+			if (ImGui::Button("test"))
+				Render();
 		}
 		ImGui::End();
 
-		ImGui::ShowDemoWindow();
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+		ImGui::Begin("View");
+		{
+			m_ViewportWidth = ImGui::GetContentRegionAvail().x;
+			m_ViewportHeight = ImGui::GetContentRegionAvail().y;
+
+			if(m_Image)
+				ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Image->GetWidth(),(float)m_Image->GetHeight() });
+		}
+		ImGui::End();
+		ImGui::PopStyleVar();
+		Render();
 	}
+	void Render()
+	{
+		Walnut::Timer timothy;
+		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight())
+		{
+			m_Image = std::make_shared<Walnut::Image>(m_ViewportWidth, m_ViewportHeight, Walnut::ImageFormat::RGBA);
+			delete[] m_ImageData;
+			m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
+		}
+
+		for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++)
+		{
+			if ((i % 2) == 0)
+				m_ImageData[i] = 0xffffff00;
+			else
+				m_ImageData[i] = Walnut::Random::UInt() | 0xff000000;
+		}
+
+		m_Image->SetData(m_ImageData);
+
+		m_ElapsedTime = timothy.ElapsedMillis();
+	}
+private:
+	std::shared_ptr<Walnut::Image> m_Image;
+	uint32_t* m_ImageData = nullptr;
+	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
+	float m_ElapsedTime = 0;
 };
 
 Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
